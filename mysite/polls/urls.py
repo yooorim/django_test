4d9d823f92233842557723211ea5262b07db072b
yooorim <yoo.gye@smail.inf.h-brs.de>
2020-12-20 22:58:26 +0100

    tutorial ing

---
+++ mysite/polls/urls.py
@@ -0,0 +1,18 @@
+polls/urls.py¶
+from django.urls import path
+from django.urls import include, path
+
+from . import views
+
+app_name = 'polls'
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
